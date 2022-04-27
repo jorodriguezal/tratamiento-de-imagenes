dac02d281580c669c936abd09616ccd7b7441bb5
judelgadoc <judelgadoc@unal.edu.co>
2022-04-27 01:05:07 -0500

    Gráficas y script. ligeros cambios en src_stb_hilos.c

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # tratamiento-de-imagenes
+
+```script_ejecutar_todo```: El script que pide el profe, los resultados los va guardando en ```results.txt```
+
+```graphs```: Script para generar el archivo ```results_graphs.txt```, que contiene las repeticiones. Esto para hacer gráficas
+
+```summarize.py```: Script para hacer un resumen de los resultados generados por ```graphs``` que se guarda en ```summary.csv```
+
+```graphs_scidavis.sciprj```: Proyecto de SciDAVis, con los resultados de ```summarize.py``` se grafica usando este software
+
+```scidavis```: Carpeta que contiene las imagenes de scidavis
+
+```input_images```: Imagenes de entrada para los experimentos
+
+```output_images```: Imagenes de salida para los experimentos
+
+```mypc_hilos```: ```src_stb_hilos.c``` pero compilado para el PC de @Juan
--- src_stb_hilos.c
+++ src_stb_hilos.c
@@ -1,37 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
-#include<pthread.h>
-#include<unistd.h>
+#include <pthread.h>
+#include <unistd.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image/stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image/stb_image_write.h"
-//%cflags:-lpthread -lm -D_DEFAULT_SOURCE
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <sys/time.h>
 
-int width, height, channels;
+
+int width, height, channels, gray_channels;
 unsigned char *input, *output;
 size_t input_size, output_size;
-int gray_channels,channels ;
 char *output_path;
 
 void read_image(char *input_path);
 void* gray_filter(void* params);
 void write_output(char *output_path, int output_channels);
 
-//Estructura de parámetros para gray_filter
-/*struct Params{
-    size_t inp;
-    size_t oup;
-    int n;
-    char *output_path;
-};*/
 
 void read_image(char *input_path) {
     input = (unsigned char *)stbi_load(input_path, &width, &height, &channels, 0);
@@ -42,24 +29,15 @@ void read_image(char *input_path) {
     printf("Loaded image with a width of %dpx, a height of %dpx and %d channels\n", width, height, channels);
 }
 
-void* gray_filter(void* i ) {
-    //struct Params *args = (struct Params*)params;
-	
+void* gray_filter(void* i) {
     int n = *(int *)i;
-    printf("n = %d", n);
     int inp= input_size*(n);
     int oup=output_size*(n);    
-    //printf("hilo %i input %i outut %i size %ld",n, inp, oup, output_size);
-    // Esto aún no lo entiendo, todo el código me lo encontré por internet
-    for(unsigned char *p = input+ inp , *pg = output+ oup ; p != input+ input_size*(n+1); p += channels, pg += gray_channels) {
+    for(unsigned char *p = input + inp, *pg = output + oup; p != input + input_size*(n+1); p += channels, pg += gray_channels) {
         *pg = (uint8_t)((*p + *(p + 1) + *(p + 2))/3.0);
-        if(channels == 4) {
+        if(channels == 4) 
             *(pg + 1) = *(p + 3);
-        }
     }
-    
-   write_output(output_path, gray_channels);
-   //free(params);
 }
 
 void write_output(char *output_path, int output_channels) {
@@ -73,7 +51,7 @@ void write_output(char *output_path, int output_channels) {
     else if (strcmp(ext, "bmp") == 0)
         stbi_write_bmp(output_path, width, height, output_channels, output);
     else {
-        printf("Output type is  not jpg, png or bmp, defaulting to output.jpg\n");
+        printf("Output type is not jpg, png or bmp, defaulting to output.jpg\n");
         stbi_write_jpg("output.jpg", width, height, output_channels, output, 100);
     }
 }
@@ -86,9 +64,11 @@ int main(int argc, char **argv)
 	gray_channels = channels == 4 ? 2 : 1;
 	input_size = width * height * channels/threads;
     output_size = width * height * gray_channels/threads;
-	int inp,oup;
+
     struct timeval tval_before, tval_after, tval_result;
     gettimeofday(&tval_before, NULL);
+
+
     output = (unsigned char *)malloc(output_size*threads);
     output_path=*(argv + 2);
     if(output == NULL) {
@@ -96,34 +76,24 @@ int main(int argc, char **argv)
         exit(1);
     }
 
-    //struct Params *params;
-    for (int i=0;i<threads;i++)
-    { 
-        //params = malloc(sizeof(struct Params));
-    	//params->inp= input_size*(i);
-    	//params->oup=output_size*(i);
-        //params->output_path=*(argv + 2);
-    	//printf("hilo %i\n",i);
+    for (int i=0; i < threads; i++) { 
         int *n = malloc(sizeof(int));
         *n = i;
-        pthread_create(&ar_threads[i], NULL, gray_filter, /*(void *)*/n);
-    	
-    	
-    	printf("fin hilo %i\n",i);
-    	//input_size += input_size;
-    	//output_size +=output_size;
+        pthread_create(&ar_threads[i], NULL, gray_filter, n);
     }
-    for (int i=0; i < threads; i++){
+
+    for (int i=0; i < threads; i++)
         pthread_join(ar_threads[i], NULL);
-    }
-	//write_output(output_path, gray_channels);
+
+	write_output(output_path, gray_channels);
+
+
     gettimeofday(&tval_after, NULL);
     timersub(&tval_after, &tval_before, &tval_result);
     printf("Seconds taken: %ld.%06ld\n", (long int)tval_result.tv_sec, (long int)tval_result.tv_usec);
 
     stbi_image_free(input);
     stbi_image_free(output);
-	
 }
 
 
